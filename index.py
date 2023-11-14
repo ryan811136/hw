27b8fd63654e435541970750d6bf6d0a2e65350a
ryan811136 <Ryan20031201@gmail.com>
2023-11-15 00:14:52 +0800

    make it better

---
+++ index.py
@@ -0,0 +1,63 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+from flask import Flask, render_template, request
+
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    X ="作者:資管二A 黃于倫 2023-11-09a<br>"
+    X +="<a href=/mis>資管導論</a><br>"
+    X +="<a href=/today>日期時間</a><br>"
+    X +="<a href=/account>帳號密碼</a><br>"
+    X +="<a href=/about>于倫網頁</a><br>"
+    X +="<a href=/welcome?guest=于倫>歡迎于倫</a><br>"
+    X +="<a href=/account>使用表單方式傳值</a><br><br>"
+    X +="<a href=/wave>人選之人演員名單(按年齡由小排到大)</a><br>"
+    return X
+
+@app.route("/mis")
+def course():
+    return "<h1>資訊管理導論</h1>"
+
+@app.route("/today")
+def today():
+    now = datetime.now()
+    return render_template("today.html",datetime = str(now))
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/welcome", methods=["GET", "POST"])
+def welcome():
+    user = request.values.get("guest")
+    return render_template("welcome.html", name=user)
+
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if request.method == "POST":
+        user = request.form["user"]
+        pwd = request.form["pwd"]
+        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
+        return result
+    else:
+        return render_template("account.html")
+
+@app.route("/wave")
+def wave():
+    Result = ""
+    db = firestore.client()
+    collection_ref = db.collection("人選之人─造浪者")    
+    docs = collection_ref.order_by("birth",direction=firestore.Query.DESCENDING).get()    
+    for doc in docs:         
+        Result += "演員：{}".format(doc.to_dict()) + "<br>"    
+    return Result
+
+if __name__ == "__main__":
+app.run()
---
+++ 網頁作業/web.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template,request
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    x ="作者:資管二A 黃于倫<br>"
+    x +="<a href=/about>我的個人簡介</a><br>"
+    x +="<a href=/account>MIS相關工作介紹</a><br>"
+    x +="<a href=/today>職涯測驗結果</a><br>"
+    x +="<a href=/welcome>未來規劃</a><br>"
+    return x
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/account")
+def account():
+    return render_template("account.html")
+
+@app.route("/today")
+def today():
+    return render_template("today.html")
+
+@app.route("/welcome")
+def welcome():
+    return render_template("welcome.html")
+
+
+#if __name__ == "__main__":
+#app.run()
